2e935deb99c2f597ba42cbe87e348e6f89dbf695
Diego Barrio H <diegobarrioh@gmail.com>
2021-12-18 13:05:06 +0100

    Todo List App con React hooks

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+
+const Button = ({ color, text, onClick }) => {
+
+    return (
+        <button className='btn' 
+                style={{backgroundColor:color}}
+                onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelBlue',
+    text: 'Button'
+}
+
+Button.protoTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
